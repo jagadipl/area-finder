273b844bd243d179d335b696350cff1b98ddf71c
Jagadip Lamsal <jagadipl@yahoo.com>
2024-01-07 20:28:00 -0500

    fixed it

--- password.js
+++ password.js
@@ -15,3 +15,38 @@ function getRandomChar(){
     random_index = Math.floor(Math.random()*length)
     return words[random_index]
 }
+ //creating random pasword
+ function randompasswordGenerator(total){
+    randompass = ""
+    for( i = 0; i<total; i++){
+        randompass+=getRandomChar()
+    }
+    console.log(`Random password is: ${randompass}`)
+ }
+ randompasswordGenerator(4)
+
+ function getRandomChar(){
+    words = "ABCDEFGH/*$#@123456"
+    length = words.length
+    randomIndex = Math.floor(Math.random()*length)
+    return words[randomIndex]
+ }
+   
+ function randompasswordgenerator(total){
+    password = ""
+    for(i = 0; i<total; i++){
+        randompass+=getRandomChar()
+        console.log(`Random password is:${randompass}`)
+
+    }
+}
+randompasswordGenerator(2)
+
+
+function getRandomchar(){
+    words = "ABCDEFGHI"
+    length = words.length
+    randomIndex = Math.floor(Math.random()*length )
+    return words[randomIndex]
+}
+
