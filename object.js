b01efaf41c0e8698139f766a5d99967a6783908b
Jagadip Lamsal <jagadipl@yahoo.com>
2024-01-07 21:10:24 -0500

    Object Section Added

---
+++ object.js
@@ -0,0 +1,10 @@
+info = {
+    "name": "jagadip",
+    "address": "USA",
+    "favMovies": ["3 Idiots", "12 Fail"]
+
+}
+
+console.log(info.favMovies[1]);
+
+// Create Objct called info and add 10 details about yourself.
